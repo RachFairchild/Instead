9fa955c290bf3501b5ebbee69f0170aafa2fcc3f
Rachel Fairchild <rfair009@gmail.com>
2022-09-22 14:06:13 -0400

    Remove cloudinary support

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,3 @@
-const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
 
@@ -30,13 +29,10 @@ module.exports = {
   },
   createPost: async (req, res) => {
     try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
 
       await Post.create({
         title: req.body.title,
         image: result.secure_url,
-        cloudinaryId: result.public_id,
         caption: req.body.caption,
         likes: 0,
         user: req.user.id,
@@ -65,8 +61,6 @@ module.exports = {
     try {
       // Find post by id
       let post = await Post.findById({ _id: req.params.id });
-      // Delete image from cloudinary
-      await cloudinary.uploader.destroy(post.cloudinaryId);
       // Delete post from db
       await Post.remove({ _id: req.params.id });
       console.log("Deleted Post");
